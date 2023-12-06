fa0afc4daf81476611b9c4598bd00d101d7f0115
Dheeraj <Dheeraj@thedijje.com>
2023-12-07 02:24:40 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cd /var/www/html/test  # Change to your application directory
+cd /var/www/html/app/  # Change to your application directory
 git pull origin main  # Pull the latest changes from the Git repository
