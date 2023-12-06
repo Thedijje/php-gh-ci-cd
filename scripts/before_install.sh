580622c8a29bc52cfb6656e046bb320f6102b3e7
Dheeraj <Dheeraj@thedijje.com>
2023-12-07 02:29:11 +0530

    Fixed and Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+chmod +x scripts/before_install.sh
+
+git config --global --add safe.directory /var/www/html/app/
+
 cd /var/www/html/app/  # Change to your application directory
 git pull origin main  # Pull the latest changes from the Git repository
