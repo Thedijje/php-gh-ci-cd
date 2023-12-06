c1bf163d3ef876018980a7bf38f28aef49db13ff
Dheeraj <Dheeraj@thedijje.com>
2023-12-07 02:34:06 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,4 +6,5 @@ export HOME=/home/ubuntu/
 git config --global --add safe.directory /var/www/html/app/
 
 cd /var/www/html/app/  # Change to your application directory
+git reset --hard
 git pull origin main  # Pull the latest changes from the Git repository
